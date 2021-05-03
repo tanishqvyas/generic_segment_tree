5397df6d7402a9fd460ebc6feaa9f81b5ad070ab
tanishqvyas <tanishqvyas069@gmail.com>
2021-05-03 17:20:37 +0530

    Added comments in Client

--- Client.cpp
+++ Client.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-using namespace std;
 #include <vector>
 #include "./src/segTree.h"
+
+using namespace std;
 
 
 int main(int argc, char const *argv[])
@@ -10,17 +11,27 @@ int main(int argc, char const *argv[])
 
     vector<int>::iterator it;
 
+  	// Create a Segment Tree for Range Sum query
     seg_tree<vector<int>::iterator, Add<int>> s(begin(v), end(v) - 1, v.size());
 
+	// Display created Segment Tree
     cout << s << "\n";
 
-    s.update(begin(v), begin(v), end(v) - 1, 1, 2, 0, 6, addEq<int>());
-    s.update(begin(v), begin(v), end(v) - 1, 3, 4, 0, 4, Eq<int>());
+    // Constant
+    int const_val1 = 6;
+    int const_val2 = 4;
+
+    // Update Query for increasing all values by 6
+    s.update(begin(v), begin(v), end(v) - 1, 1, 2, 0, const_val1, addEq<int>());
+
+    // Update query to set all values in the range as 4
+    s.update(begin(v), begin(v), end(v) - 1, 3, 4, 0, const_val2, Eq<int>());
 
     cout << s << "\n";
 
+    // Performing Range Sum Query
     auto val = s.query(v.begin(), v.begin(), end(v) - 1, 1, 4, 0);
-    cout << val << " ---\n";
+    cout << val << "\n";
 
     return 0;
 }
